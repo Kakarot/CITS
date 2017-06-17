4e3ccf0aa0da2be36a11b2f3459bdcf7d523fc01
jruiz30 <jruiz30@gatech.edu>
2017-06-17 16:46:11 -0400

    Created ResultsPage to hold stats data after student finishes Quiz. Added modality UI. Added event listeners to detect close event.

--- CITS/ResultsPage.xaml.cs
+++ CITS/ResultsPage.xaml.cs
@@ -7,9 +7,19 @@ namespace CITS
 {
     public partial class ResultsPage : ContentPage
     {
+        
+    public event EventHandler ResultsScreenDismissed;
+    
         public ResultsPage()
         {
             InitializeComponent();
         }
+
+		async void OnDismissButtonClicked(object sender, EventArgs args)
+		{			
+            ResultsScreenDismissed(this, EventArgs.Empty);
+            await Navigation.PopModalAsync();
+		}
+	
     }
 }
