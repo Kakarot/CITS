c4dad7bb01348b590f1621028cdbf39f99943d9c
jruiz30 <jruiz30@gatech.edu>
2017-06-17 16:46:18 -0400

    Created ResultsPage to hold stats data after student finishes Quiz. Added modality UI. Added event listeners to detect close event.

--- CITS/OrderOfOperationsPage.xaml.cs
+++ CITS/OrderOfOperationsPage.xaml.cs
@@ -6,10 +6,34 @@ using Xamarin.Forms;
 namespace CITS
 {
     public partial class OrderOfOperationsPage : ContentPage
-    {
+    {      
         public OrderOfOperationsPage()
         {
             InitializeComponent();
         }
+
+        async void Handle_Clicked_Start_Quiz(object sender, System.EventArgs e)
+        {
+           await DisplayAlert("Sorry!", "The quiz feature has not " +
+                         "been implemented yet, please check if new build " +
+                         "has it!",
+                        "OK");
+
+            //TODO: Instantiate Quiz Model
+            //TODO: Generate List<MathProblemModel>
+            //TODO: Ensure last page is ResultsPage, Pass QuizModel
+
+            var resultsPage = new ResultsPage();
+
+            //Subscribe to page's close event
+            resultsPage.ResultsScreenDismissed += QuizCompleted;
+
+           await Navigation.PushModalAsync(resultsPage);		
+        }
+
+		public void QuizCompleted(object sender, EventArgs e)
+		{
+			DisplayAlert("Congrats!", "Great job finishing Quiz!", "OK");
+		}
     }
 }
