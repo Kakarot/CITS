2fb4108ac81f131d4ea33f329fddfd16ad90b7e8
jruiz30 <jruiz30@gatech.edu>
2017-06-25 16:34:19 -0400

    Page that is constantly updated for each problem given to it. Implemented input controls for answer submissions.

--- CITS/ProblemPage.xaml.cs
+++ CITS/ProblemPage.xaml.cs
@@ -7,9 +7,46 @@ namespace CITS
 {
     public partial class ProblemPage : ContentPage
     {
+        public event EventHandler SolutionSubmitted;
+        public String ProblemNumber
+        {
+            get;
+            set;
+        }
+		public String Problem
+		{
+			get;
+			set;
+		}
+
+        public String Solution
+        {
+            get;
+            set;
+        }
+
         public ProblemPage()
         {
             InitializeComponent();
+        }
+
+        void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+         //   throw new NotImplementedException();
+        }
+
+		void OnSubmitButtonClicked(object sender, EventArgs args)
+		{
+            this.Solution = SolutionEntry.Text;
+            SolutionSubmitted(this, EventArgs.Empty);         
+			//await Navigation.PopModalAsync();
+		}
+
+       public void UpdateProblemPage()
+        {
+            ProblemNumberLabel.Text = ProblemNumber;
+            ProblemLabel.Text = Problem;
+            SolutionEntry.Text="";
         }
     }
 }
