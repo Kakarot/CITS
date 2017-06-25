70b5f7faf709f8af4bf4e1afa6a518b4f7ea2793
jruiz30 <jruiz30@gatech.edu>
2017-06-25 16:33:51 -0400

    Added ProblemPage to project

---
+++ CITS/ProblemPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CITS
+{
+    public partial class ProblemPage : ContentPage
+    {
+        public ProblemPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
