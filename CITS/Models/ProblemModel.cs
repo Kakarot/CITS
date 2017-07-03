a7b0708f1e1a87765c5ad12645bed9a23ef9bab3
jruiz30 <jruiz30@gatech.edu>
2017-07-02 22:40:17 -0400

    Support list of hints per problem

--- CITS/Models/ProblemModel.cs
+++ CITS/Models/ProblemModel.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace CITS.Models
 {
     public class ProblemModel : IProblem
     {
-        public ProblemModel(string Problem, string Solution) 
+        public ProblemModel(string Problem, string Solution, List<String> listOfHints) 
         {
             this.Problem = Problem;
             this.Solution = Solution;
+            this.Hints = listOfHints;
         }
 
         public string Problem { get; set; }
         public string Solution { get; set; }
-
+        public List<String> Hints { get; set; }
 
 
         public virtual bool IsSolutionCorrect(string candidateSolution)
