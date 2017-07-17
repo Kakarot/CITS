34c467c20a930a148b2721406fe83b82a3f09067
jruiz30 <jruiz30@gatech.edu>
2017-07-16 23:07:01 -0400

    Addded ConfidenceEventArgs and EmotionEventArgs to project

---
+++ CITS/ConfidenceEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CITS
+{
+    public class ConfidenceEventArgs
+    {
+        public ConfidenceEventArgs()
+        {
+        }
+    }
+}
---
+++ CITS/EmotionEventArgs.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace CITS
+{
+    public class ConfidenceEventArgs : EventArgs
+    {
+        public double Anger
+        {
+            get;
+            set;
+        }
+		public double Contempt
+		{
+			get;
+			set;
+		}
+
+		public double Disgust
+		{
+			get;
+			set;
+		}
+
+		public double Fear
+		{
+			get;
+			set;
+		}
+
+		public double Happiness
+		{
+			get;
+			set;
+		}
+
+		public double Neutral
+		{
+			get;
+			set;
+		}
+
+		public double Sadness
+		{
+			get;
+			set;
+		}
+
+		public double Surprise
+		{
+			get;
+			set;
+		}
+
+		public ConfidenceEventArgs(float anger, float contempt, float disgust,
+                                  float fear, float happiness, float neutral,
+                                  float sadness, float surprise)
+        {
+            this.Anger = anger;
+            this.Contempt = contempt;
+            this.Disgust = disgust;
+            this.Fear = fear;
+            this.Happiness = happiness;
+            this.Neutral = neutral;
+            this.Sadness = sadness;
+            this.Surprise = surprise;
+        }
+    }
+}
