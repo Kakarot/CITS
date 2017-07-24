a05a6b225ef8e82e37a8215a9dad2d34b1896d0b
jruiz30 <jruiz30@gatech.edu>
2017-07-23 22:37:11 -0400

    Changed confidence definition to be if positiveEmotions > negativeEmotions

--- CITS/OrderOfOperationsPage.xaml.cs
+++ CITS/OrderOfOperationsPage.xaml.cs
@@ -226,7 +226,9 @@ namespace CITS
             //is being shown and we do not need to generate the last problem.
             if(MathProblems.Count != 0)
 			{
-			    if(!quiz.WasPreviousAnswerCorrect /*&& quiz.GenerateNewProblemBasedOnConfidence(e)*/)
+                //Generate a new problem if the answer is incorrect, or 
+                //if the user got the answer correct but was not confident
+                if(!quiz.WasPreviousAnswerCorrect || !quiz.IsUserConfident(e))
 			    {  
 					var newProblemQueue = quiz.GenerateMathProblems(1);
 					MathProblems.Enqueue(newProblemQueue.Dequeue());
